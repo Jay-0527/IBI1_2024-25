368ece5bbb272c1027a53f9a92752860ccfe9624
Song ziyang <ziyang.24@intl.zju.edu.cn>
2025-03-19 15:44:37 +0800

    change

--- Practical5/bar_chart.py
+++ Practical5/bar_chart.py
@@ -1,16 +1,20 @@
+#Set a dictionary to record data and print it.
+#Set a variable that can be changed to output the value.
+#Import matplotlib to draw chart. Set languages and their corresponding values.
+#Make colours different in different bars and define ylabel and the title. Show the exact value on top of each bar.
 dict1={"JavaScript":62.3,"HTML":52.9,"Python":51,"SQL":51,"TypeScript":38.5}
 print(dict1)
-percentage=dict1["HTML"]
+percentage=dict1["HTML"] #Set a variable
 print(percentage)
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
 
 languages = ['JavaScript', 'HTML', 'Python', 'SQL', 'TypeScript']
-counts = [62.3, 52.9, 51, 51, 38.5]
-bar_colors = ['tab:red', 'tab:blue', 'tab:pink', 'tab:orange', 'tab:green']
+counts = [62.3, 52.9, 51, 51, 38.5] #Set languages and their corresponding values.
+bar_colors = ['tab:red', 'tab:blue', 'tab:pink', 'tab:orange', 'tab:green'] #Set different colours in different bars.
 bars=ax.bar(languages, counts, color=bar_colors)
 ax.set_ylabel('percentage')
 ax.set_title('percentage of users')
-ax.bar_label(bars, labels=counts)
+ax.bar_label(bars, labels=counts)#Show the exact value on top of bar
 plt.show()
--- Practical5/pie_chart.py
+++ Practical5/pie_chart.py
@@ -1,32 +1,38 @@
+#Set two lists for both uk_countries and cn_provinces and sort them
+#Import matplotlib to draw pie chart.
+#Set a recipen to record the value. Use data and gredient to extract them.
+#Define a function to format the data. Darw the pie chart using ax.pie and set a colour.
+#Make an illustration to show the corresponding relationship.
+#Set the size of the chart and make the words bold.
 uk_countries=[57.11,3.13,1.91,5.45]
 cn_provinces=[65.77, 41.88, 45.28, 61.27, 85.15]
 sort_uk_couintries=sorted(uk_countries)
-sort_cn_provinces=sorted(cn_provinces)
+sort_cn_provinces=sorted(cn_provinces)#Sort the values and print them
 print(f"countries in UK:{sort_uk_couintries}")
 print(f"Zhejiang neighbouring provinces:{sort_cn_provinces}")
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal")) #Set the figure size and make sure it is a pie.
 
 recipe1 = ["57.11 billion England",
           "3.13 billion Wales",
           "1.91 billion Northern_Ireland",
-          "5.45 billion Scotland"]
+          "5.45 billion Scotland"] #Store the data in list
 
 data = [float(x.split()[0]) for x in recipe1]
-ingredients = [x.split()[-1] for x in recipe1]
+ingredients = [x.split()[-1] for x in recipe1] #Extract data and ingredients from recipe.
 def func(pct, allvals):
-    absolute = int(np.round(pct/100.*np.sum(allvals)))
-    return f"{pct:.1f}%\n({absolute:d} billion)"
+    accurate = float((pct/100.*sum(allvals)))
+    return f"{pct:.1f}%\n({accurate:.2f} billion)"#Format the value
 wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
-                                  textprops=dict(color="w"))
+                                  textprops=dict(color="w")) #Draw the pie chart and set the colour white.
 ax.legend(wedges, ingredients,
           title="Ingredients",
           loc="center left",
-          bbox_to_anchor=(1, 0, 0.5, 1))
+          bbox_to_anchor=(1, 0, 0.5, 1)) #Make an illustration next to the pie chart
 
-plt.setp(autotexts, size=8, weight="bold")
+plt.setp(autotexts, size=8, weight="bold") #Set size and font.
 ax.set_title("Countries in UK")
 plt.show()
 
