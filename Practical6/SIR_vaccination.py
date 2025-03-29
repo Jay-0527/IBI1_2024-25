e2d77ad71b839ec951ac72d92133740924217181
Song ziyang <ziyang.24@intl.zju.edu.cn>
2025-03-29 16:56:59 +0800

    Initialism

--- Practical6/SIR_vaccination.py
+++ Practical6/SIR_vaccination.py
@@ -1,40 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
+from matplotlib import cm #import certain libraries
 N=10000
-vaccine_percentage=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-for k in vaccine_percentage:
+vaccine_percentage=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1] #make a list for vaccine percentage.
+for k in vaccine_percentage: #use for loop to traverse every value in the list
     vaccine=int(N*k)
     infected=1
     if N-infected-vaccine>0:
         susceptible=N-infected-vaccine
     else:
-        susceptible=0
+        susceptible=0 #Make sure that the value is positive for every k
     
     recovered=0
     beta=0.3
-    gama=0.05
+    gama=0.05 #set variables for given information
     S=[susceptible]
     I=[infected]
-    R=[recovered]
+    R=[recovered] #Create lists to record the number of people who are susceptible, infected or recovered.
     
-    for i in range (1000):
+    for i in range (1000): #set a 1000 times loops
         infected_probability=beta*I[-1]/N
         updated_infected=np.random.choice(range(2),S[-1],p=[1-infected_probability,infected_probability]).sum()
-        updated_recovery=np.random.choice(range(2),I[-1],p=[1-gama,gama]).sum()
+        updated_recovery=np.random.choice(range(2),I[-1],p=[1-gama,gama]).sum() #calculate the number of people who are infected right now or recovered right now
         
         actual_susceptible=S[-1]-updated_infected
         actual_infected=I[-1]+updated_infected-updated_recovery
-        actual_recovered=R[-1]+updated_recovery
+        actual_recovered=R[-1]+updated_recovery #calculate the actual number of people who are susceptible, infected, recovered for now.
         S.append(actual_susceptible)
         I.append(actual_infected)
-        R.append(actual_recovered)
-    color_value = int(255*k)
-    plt.plot(I, label=f'{int(k*100)}% vaccinated', color=cm.viridis(color_value))
+        R.append(actual_recovered) #add the actual number to the list
+    color_value = int(255*k) #Set gradients colour when k increases
+    plt.plot(I, label=f'{int(k*100)}% vaccinated', color=cm.viridis(color_value)) #draw the plot and add the label corresponding to the k value at present
         
     
 plt.xlabel('time')
 plt.ylabel('number of people')
 plt.title('SIR model with different vaccination rate')
-plt.legend()
+plt.legend() #Set x,y label, title and legend for the chart.
 plt.show()
--- Practical6/spatial_SIR.py
+++ Practical6/spatial_SIR.py
@@ -6,18 +6,22 @@ population[outbreak[0], outbreak[1]] = 1 #choose one random point in our 100 100
 plt.figure (figsize =(6,4),dpi=150) 
 beta=0.3
 gama=0.05 #set variables for given information
-snapshot_times = [0, 10, 50, 99] #make a list to record the four time for drawing picture
-for t in range(100): #make a 100 times loop
-    infected_area=np.where(population==1) # return a tuple for the coordinates of infected areas
-    infected_points = list(zip(infected_area[0], infected_area[1])) #change the tuple to the list that record every eligible coordinates.
-    for (i, j) in infected_points: # Check all 8 neighbors
-            for x in [i-1, i, i+1]:
-                for y in [j-1, j, j+1]: 
-                    if (x == i and y == j) or x < 0 or y < 0 or x >= 100 or y >= 100:
-                        continue # jump the loop to make sure the eight neighbours are in the field
-                    
-                    if population[x, y] == 0 and np.random.random() < beta:
-                        population[x, y] = 1 #Infect susceptible neighbors according to probability beta
+snapshot_times = [0, 10, 50, 100] #make a list to record the four time for drawing picture
+for t in range(101): # make a 100 times loop
+    if t==0:
+        infected_area=np.where(population==1) # return a tuple for the coordinates of infected areas
+        infected_points = list(zip(infected_area[0], infected_area[1])) #change the tuple to the list that record every eligible coordinates.
+    else:
+        infected_area=np.where(population==1) # return a tuple for the coordinates of infected areas
+        infected_points = list(zip(infected_area[0], infected_area[1])) #change the tuple to the list that record every eligible coordinates.
+        for (i, j) in infected_points: # Check all 8 neighbors
+                for x in [i-1, i, i+1]:
+                    for y in [j-1, j, j+1]: 
+                        if (x == i and y == j) or x < 0 or y < 0 or x >= 100 or y >= 100:
+                            continue # jump the loop to make sure the eight neighbours are in the field
+                        
+                        if population[x, y] == 0 and np.random.random() < beta:
+                            population[x, y] = 1 # Infect susceptible neighbors according to probability beta
     for (i, j) in infected_points:
             if np.random.random() < gama:
                 population[i, j] = 2 #make the infected person recovered according to gama
