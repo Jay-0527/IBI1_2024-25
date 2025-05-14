f9531b0f050c2d4a14b0e606849c0a45ca095805
Song ziyang <ziyang.24@intl.zju.edu.cn>
2025-05-14 14:56:16 +0800

    Initialism

--- Practical13/alignment.py
+++ Practical13/alignment.py
@@ -1,29 +1,27 @@
-#length of amino acids: 222
-#subcellular location: Mitochondrial Matrix
-#hits: 250
-#percentage identities: 58.1%~100%
-
-from Bio import SeqIO
-from Bio.Align import substitution_matrices
+from Bio import SeqIO # import SeqIO module from Bio package
+from Bio.Align import substitution_matrices # import certain substitution matrices
 
 def calculate_alignment_score(seq1, seq2):
-    blosum62 = substitution_matrices.load("BLOSUM62")
-    print(blosum62)
-    score = 0
-    for aa1, aa2 in zip(seq1, seq2):
-        score += blosum62[aa1][aa2]
+    blosum62 = substitution_matrices.load("BLOSUM62") # load BLOSUM62 matrix
+    seq1 = seq1.upper() # convert sequences to uppercase
+    seq2 = seq2.upper()
+    score = 0  # Set initialize score to 0.
+    for aa1, aa2 in zip(seq1, seq2): # iterate over the sequences and calculate the score.
+        score += blosum62[aa1][aa2] # add the score of the current pair of amino acids to the total score.
     return score
 
 def calculate_identity_percentage(seq1, seq2):
-    identical = 0
-    for aa1, aa2 in zip(seq1, seq2):
+    identical = 0 # Set initialize identical to 0.
+    seq1 = seq1.upper() # convert sequences to uppercase
+    seq2 = seq2.upper()
+    for aa1, aa2 in zip(seq1, seq2): # iterate over the sequences and calculate the number of identical amino acids.
         if aa1 == aa2:
-            identical += 1
-    return (identical / len(seq1)) * 100
+            identical += 1 # add 1 to the number of identical amino acids if the current pair of amino acids are identical.
+    return (identical / len(seq1)) * 100 # calculate and return the identity percentage.
 
-human_seq = SeqIO.read(r"C:\Users\71588\Desktop\IBI1_2024-25\Practical13\human.fasta", "fasta")
-mouse_seq = SeqIO.read(r"C:\Users\71588\Desktop\IBI1_2024-25\Practical13\mouse.fasta", "fasta")
-ransom_seq = SeqIO.read(r"C:\Users\71588\Desktop\IBI1_2024-25\Practical13\random.fasta", "fasta")
+human_seq = SeqIO.read(r"C:\Users\71588\Desktop\IBI1_2024-25\Practical13\human.fasta", "fasta") # read in the human sequence.
+mouse_seq = SeqIO.read(r"C:\Users\71588\Desktop\IBI1_2024-25\Practical13\mouse.fasta", "fasta") # read in the mouse sequence.
+ransom_seq = SeqIO.read(r"C:\Users\71588\Desktop\IBI1_2024-25\Practical13\random.fasta", "fasta") # read in the random sequence.
 
 #human vs mouse
 print("Human vs Mouse")
